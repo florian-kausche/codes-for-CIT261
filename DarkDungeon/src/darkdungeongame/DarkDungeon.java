3e5df2d6d3f1135cf18d95cbf18f65b1c962b5a4
Grzegorz Kenar <gregg@10.0.1.11>
2016-05-10 12:37:44 -0500

    5/10/2016 Added Item class

--- DarkDungeon/src/darkdungeongame/DarkDungeon.java
+++ DarkDungeon/src/darkdungeongame/DarkDungeon.java
@@ -10,7 +10,7 @@ import byui.cit260.darkdungeon.model.*;
  */
 public class DarkDungeon {
     public static void main(String[] args) {
-    //test the player class
+    //test the player class - Everyone
     System.out.println("This is a test in for the player class:");    
     Player playerOne = new Player();
     playerOne.setName("Goku");
@@ -18,10 +18,16 @@ public class DarkDungeon {
     String playerInfo = playerOne.toString();
     System.out.println(playerInfo+"\n");
     
-    //test the character class
+    //test the character class - Greg
     System.out.println("This is a test in for the character class:");
     Characters warrior = new Characters("Warrior",100,10,50,10,30,50,50);
     String characterInfo = warrior.toString();
     System.out.println(characterInfo+"\n");
+    
+    //test the items class - Greg
+    System.out.println("This is a test in for the item class:");
+    Items items = new Items("Potion of Life","Potion",0,35,3);
+    String itemInfo = items.toString();
+    System.out.println(itemInfo+"\n");
     }
 }
